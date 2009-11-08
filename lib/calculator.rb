6e82a2a5e3190ef3d4b6da857471e4eb5b9e5308
Mario Aquino <mario.e.aquino@gmail.com>
2009-11-08 16:12:18 -0600

    Improvement on functional approach.  No mutable state.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,13 +3,17 @@ module Calculator
     raise ArgumentError, 'Must be a string expression' unless expression.is_a? String
     raise ArgumentError, 'Must be numeric arithmetic' unless expression =~ pattern
     
-    sets = expression.scan(pattern)
-    sets.reduce(eval(*sets.shift)) {|memo, obj| eval(memo, *obj[1..2]) }
+    sets = lambda { expression.scan(pattern) }
+    
+    sets.call.drop(1).
+      reduce(evaluate(*sets.call.first)) do |last_result, equation| 
+        evaluate(last_result, *equation[1..2]) 
+      end
   end
   
   private
-  def self.eval(left, operator, right)
-    Kernel::eval(left + operator + right).to_s
+  def self.evaluate(left, operator, right)
+    eval(left + operator + right).to_s
   end
   
   def self.pattern
